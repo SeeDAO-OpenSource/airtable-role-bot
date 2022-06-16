7aa91a1d9b6df0ba855a117e23abec0e41b606df
fatfingererr <fatfingererr@gmail.com>
2022-06-16 21:06:27 +0800

    添加 GUILD_MEMBERS intents 才能取得服务器所有人的资料

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,14 @@ const fs = require('fs');
 const { Client, Intents } = require('discord.js');
 
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({
+    intents:
+        [Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MEMBERS]
+});
+
 
 client.on('ready', async () => {
     const channel = client.channels.cache.find((channel) => channel.name === config.channel);
@@ -16,21 +23,28 @@ client.on('ready', async () => {
     console.log(`机器人成功启动, 监听频道: #'${JSON.stringify(channel.name)}\n`)
 });
 
-client.on('messageCreate', msg => {
+client.on('messageCreate', async (msg) => {
     if (!msg.content.startsWith(config.prefix) || msg.author.bot) return;
     const args = msg.content.slice(config.prefix.lenght).split(/ +/)
     const userName = args[1]
     const roleName = args[2]
 
+    const guild = client.guilds.cache.get(config.discordId);
     const user = client.users.cache.find(u => u.tag == userName);
-    const guild = client.guilds.cache.get(msg.guildId);
+    const channel = client.channels.cache.get(msg.channelId);
+    channel.send(`同步 AirTable 最新测验信息 ...`);
+
+    if (user === undefined) {
+        console.error(`User not found for '${userName}'`);
+        channel.send(`⚠️ 找不到成员 '${userName}', 请联络 SeeDAO 管理员`);
+        return;
+    }
+
     const member = guild.members.cache.get(user.id);
     const role = guild.roles.cache.find((role) => {
         return role.name === config.roles[roleName];
     });
 
-    const channel = client.channels.cache.get(msg.channelId);
-    channel.send(`同步 AirTable 最新测验信息 ...`);
     if (!role) {
         console.error(`Role not found for '${roleName}'`);
         channel.send(`⚠️ 找不到身份标签 '${roleName}', 请联络 SeeDAO 管理员`);
