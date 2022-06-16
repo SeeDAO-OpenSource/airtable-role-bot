df426aa3c060f19f740b032a3a3fde099c9924da
fatfingererr <fatfingererr@gmail.com>
2022-06-16 20:03:26 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# AirTable 给身份标签 Discord 机器人
+
+TBD
+
+## 快速开始
+
+1. 需要使用 Node 版本 `v16.6.0` 才能使用 `discord.js`, 使用 NVM 选择版本:
+
+```
+nvm install v16.6.0
+nvm use v16.6.0
+npm install ts-node
+```
---
+++ src/index.ts
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const fs = require('fs');
+const { Client, Intents } = require('discord.js');
+
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', async () => {
+    const channel = client.channels.cache.find((channel) => channel.name === config.channel);
+    try {
+        await channel.messages.fetch();
+    } catch (err) {
+        console.error('Error fetching channel messages', err);
+        return;
+    }
+    console.log(`机器人成功启动, 监听频道: #'${JSON.stringify(channel.name)}\n`)
+});
+
+client.on('messageCreate', msg => {
+    if (!msg.content.startsWith(config.prefix) || msg.author.bot) return;
+    const args = msg.content.slice(config.prefix.lenght).split(/ +/)
+    const userName = args[1]
+    const roleName = args[2]
+
+    const user = client.users.cache.find(u => u.tag == userName);
+    const guild = client.guilds.cache.get(msg.guildId);
+    const member = guild.members.cache.get(user.id);
+    const role = guild.roles.cache.find((role) => {
+        return role.name === config.roles[roleName];
+    });
+
+    const channel = client.channels.cache.get(msg.channelId);
+    channel.send(`同步 AirTable 最新测验信息 ...`);
+    if (!role) {
+        console.error(`Role not found for '${roleName}'`);
+        channel.send(`⚠️ 找不到身份标签 '${roleName}', 请联络 SeeDAO 管理员`);
+        return;
+    }
+    try {
+        member.roles.add(role.id);
+        channel.send(`🎉 恭喜 ${user.toString()} 通过 OnBoarding 测验, 取得 **'${roleName}'** 身份标签!`);
+    } catch (err) {
+        console.error('Error adding role', err);
+        channel.send(`⚠️ 添加身份 '${roleName}' 失败, 请联络 SeeDAO 管理员`);
+        return;
+    }
+})
+
+client.login(process.env.BOT_TOKEN);
